a2bf0d3419cb5bab4b58d14c9a5427e8c671902c
sseungmn <severck12@naver.com>
2022-04-15 20:11:30 +0900

    build : basic settings

---
+++ ChuckNorrisRandomJoke/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ChuckNorrisRandomJoke
+//
+//  Created by seungminOH on 2022/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
